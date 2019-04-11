abe841c0800497b7251fe0842ad83cdcae7f8a71
tanvir-kaur <tanvirkaur1691@gmail.com>
2019-04-11 23:15:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,49 +2,52 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mailgun = require('mailgun-js')({ 
+const mailgun = require('mailgun-js')({
     apiKey: '305c50803daa3afeacdac228cf6fe5ed-7bbbcb78-9bd5022c',
-    domain: 'sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org' 
-    });
-    
+    domain: 'sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org'
+});
+
 const data = {
-        from: 'postmaster@sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org',
-        to: 'tkaur@covalience.com',
-        subject: 'Hello',
-        text: 'Testing some Mailgun awesomeness!'
-    };
+    from: 'postmaster@sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org',
+    to: 'tkaur@covalienc.com',
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomeness!'
+};
 
 const restService = express();
 
 restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 
 restService.use(bodyParser.json());
 
 restService.post("/echo", function (req, res) {
-  var email =  req.body.result && req.body.result.parameters && req.body.result.parameters.email ? req.body.result.parameters.email : "Empty response";
-  var returnString='';
-  mailgun.messages().send(data, (error, body) => {
-    returnString=body;
-  });
+    var email = req.body.result && req.body.result.parameters && req.body.result.parameters.email ? req.body.result.parameters.email : "no email in response";
+    var name = req.body.result && req.body.result.parameters && req.body.result.parameters.name ? req.body.result.parameters.name : "No name in response";
 
-  return res.json({
-    "fulfillmentText":'hello'
-    ,"fulfillmentMessages":[
-        {
-            "text": {
-                "text": [
-                    "Hello I m Responding to intent"
-                ]
+    mailgun.messages().send(data, (error, body) => {
+        console.log(body);
+    });
+
+
+
+    return res.json({
+        "fulfillmentText": email + "" + name
+        , "fulfillmentMessages": [
+            {
+                "text": {
+                    "text": [
+                        "Okay,our representative will email you shortly !"
+                    ]
+                }
             }
-        }
-    ]
-  });
+        ]
+    });
 });
 
 restService.listen(process.env.PORT || 8000, function () {
-  console.log("Server up and listening");
+    console.log("Server up and listening");
 });
