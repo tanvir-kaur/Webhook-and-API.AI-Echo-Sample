b57ca1772a56492a85e422da5d7ea8cf1e33c9f7
tanvir-kaur <tanvirkaur1691@gmail.com>
2019-04-11 18:45:15 +0530

    update

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
     req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
+    req.body.result.parameters.email
+      ? req.body.result.parameters.email
+      : req.body.result;
   return res.json({
     speech: speech,
     displayText: speech,
