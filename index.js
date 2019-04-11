ca4c22b90bae7e0fb1040dbf7a9d96d1c6ce8681
tanvir-kaur <tanvirkaur1691@gmail.com>
2019-04-11 22:55:14 +0530

    test

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mailgun = require('mailgun-js')({ 
+    apiKey: '305c50803daa3afeacdac228cf6fe5ed-7bbbcb78-9bd5022c',
+    domain: 'sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org' 
+    });
+    
+const data = {
+        from: 'postmaster@sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org',
+        to: 'tkaur@covalienc.com',
+        subject: 'Hello',
+        text: 'Testing some Mailgun awesomeness!'
+    };
 
 const restService = express();
 
@@ -14,31 +25,15 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function (req, res) {
-  var email =
-    req.body.result &&
-      req.body.result.parameters &&
-      req.body.result.parameters.email
-      ? req.body.result.parameters.email
-      : "Empty response";
-
-      var API_KEY = '305c50803daa3afeacdac228cf6fe5ed-7bbbcb78-9bd5022c';
-      var DOMAIN = 'sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org';
-      var mailgun = require('mailgun-js')({ apiKey: API_KEY, domain: DOMAIN });
-
-        const data = {
-          from: 'postmaster@sandboxa466a01624994ab79d8e05f6a35e69a1.mailgun.org',
-          to: 'tkaur@covalienc.com',
-          subject: 'Hello',
-          text: 'Testing some Mailgun awesomeness!'
-        };
-
+  var email =  req.body.result && req.body.result.parameters && req.body.result.parameters.email ? req.body.result.parameters.email : "Empty response";
+  var returnString='';
   mailgun.messages().send(data, (error, body) => {
-    console.log(body);
+    returnString=body;
   });
 
   return res.json({
-    speech: email,
-    displayText: email,
+    speech: 'test',
+    displayText: returnString,
     source: "webhook-echo-sample"
   });
 });
